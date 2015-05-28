43449ecf21b54a646f36acf162395fcca542c1cf
Koen <koen.vd@hotmail.com>
2015-05-28 16:23:06 +0200

    Updated summonerInfoService to use the server and not the riot api directly

--- app/services/summoner_info/summonerInfoService.js
+++ app/services/summoner_info/summonerInfoService.js
@@ -6,8 +6,11 @@ angular.module('leagueApp.service.summonerInfo', ['ngResource'])
 summonerInfoService.$inject = ['$resource'];
 
 function summonerInfoService($resource) {
-    var summonerInfo = $resource('https://euw.api.pvp.net/api/lol/:region/:version/summoner/by-name/:summonerName', {}, {
+/*    var summonerInfo = $resource('https://euw.api.pvp.net/api/lol/:region/:version/summoner/by-name/:summonerName', {}, {
         'get': {method: 'GET', params: {version: 'v1.4', 'api_key': '3c343a96-21ad-41bf-9d65-dd608a65d6ec'}}
+    });   */
+    var summonerInfo = $resource('api/:region/summoner/:summonerName', {}, {
+        'get': {method: 'GET'}
     });
 
     return {
