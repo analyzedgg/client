68970c7d8c104609c0de7600ac67e7de0b764904
KoenD <koen.vd@hotmail.com>
2015-05-28 16:24:20 +0200

    Moved the stateServiceSpec to the same folder as the service itself

---
+++ app/services/stateServiceSpec.js
@@ -0,0 +1,43 @@
+describe('StateService', function () {
+
+    var stateService;
+    beforeEach(module('leagueApp'));
+    beforeEach(inject(function ($provide) {
+        $provide.value('StateService', stateService);
+    }));
+
+    //Must
+    describe('the handling of summoner data', function () {
+        var baseSummoner = {name: 'minikoen', id: '123'};
+        beforeEach(function () {
+            stateService.setActiveSummoner(baseSummoner);
+        });
+
+        it('should retrieve the active summoner data', function () {
+            expect(stateService.getActiveSummoner()).toBe(baseSummoner);
+        });
+
+        it('should set the active summoner data', function () {
+            var newSummoner = {name: 'wagglez', id: '321'};
+            stateService.setActiveSummoner(newSummoner);
+            expect(stateService.getActiveSummoner()).toBe(newSummoner);
+        });
+
+    });
+
+    describe('the handling of region data', function () {
+        var baseRegion = 'euw';
+        beforeEach(function () {
+            stateService.setActiveRegion(baseRegion);
+        });
+
+        it('should retrieve the active region data', function () {
+            expect(stateService.getActiveRegion()).toBe(baseRegion);
+        });
+
+        it('should set the active region data', function () {
+            stateService.setActiveRegion('na');
+            expect(stateService.getActiveRegion()).toBe('na');
+        })
+    });
+});
