ab3e58aea688131356aa034e8c2188abef809f0d
Koen <koen.vd@hotmail.com>
2015-05-28 07:23:18 +0200

    Updated summoner page html + js

--- app/summoner_page/summoner_page.js
+++ app/summoner_page/summoner_page.js
@@ -1,55 +1,56 @@
 'use strict';
 
-angular.module('leagueAPIApp.summoner_page', ['ngRoute'])
+angular.module('leagueApp.summoner_page', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/summoner', {
             templateUrl: 'summoner_page/summoner_page.html',
-            controller: 'SummonerPageCtrl'
+            controller: 'SummonerPageCtrl',
+            controllerAs: 'summoner'
         });
     }])
+    .controller('SummonerPageCtrl', summonerController);
 
-    .controller('SummonerPageCtrl', ['$scope', '$q', 'SummonerInfoService', 'StatisticsService', 'ChampionService', 'StateService', function ($scope, $q, summonerInfoService, statisticsService, championService, stateService) {
+summonerController.$inject = ['$scope', 'SummonerInfoService', 'StateService'];
 
-        $scope.regions = [
-            {name: 'West Europe', code: 'euw'},
-            {name: 'North America', code: 'na'},
-            {name: 'Korea', code: 'kr'},
-            {name: 'Brazil', code: 'br'},
-            {name: 'Europe Nordic & East', code: 'eune'},
-            {name: 'Latin America South', code: 'las'},
-            {name: 'Oceania', code: 'oce'},
-            {name: 'Russia', code: 'rus'},
-            {name: 'Turkey', code: 'tr'}
-        ];
+function summonerController($scope, summonerInfoService, stateService) {
+    var summoner = this;
+    summoner.regions = [
+        {name: 'West Europe', code: 'euw'},
+        {name: 'North America', code: 'na'},
+        {name: 'Korea', code: 'kr'},
+        {name: 'Brazil', code: 'br'},
+        {name: 'Europe Nordic & East', code: 'eune'},
+        {name: 'Latin America South', code: 'las'},
+        {name: 'Oceania', code: 'oce'},
+        {name: 'Russia', code: 'rus'},
+        {name: 'Turkey', code: 'tr'}
+    ];
 
-        $scope.usernameInput = 'Minikoen';
+    summoner.usernameInput = 'Minikoen';
 
-        $scope.retrievePageData = function () {
-            var activeRegion = stateService.getActiveRegion();
-            var summonerName = $scope.usernameInput;
+    summoner.retrievePageData = function () {
+        var activeRegion = stateService.getActiveRegion();
+        var summonerName = summoner.usernameInput;
 
-            $scope.summonerData = summonerInfoService.getSummoner(activeRegion, summonerName);
+        getSummonerInfo(activeRegion, summonerName);
 
-        };
+    };
 
-        function getSummonerInfo(region, summonerName) {
-            console.log('Gonna perform the request for summoner info with the follow params:', summonerName, region);
-            return summonerInfoService.get({userNames: summonerName, region: region},
-                function (response) {
-                    $scope.summonerData = response;
-                    stateService.setActiveSummoner(response[angular.lowercase(summonerName)]);
-                },
-                function (errorResponse) {
-                    console.log('Something went wrong with the response for summoner info', errorResponse);
-                });
-        }
-
-        $scope.$watch('regionCodeInput', function (newRegion, oldRegion) {
-            console.log('Region changed from', oldRegion, 'to', newRegion);
-            stateService.setActiveRegion(newRegion);
+    function getSummonerInfo(region, summonerName) {
+        console.log('Gonna perform the request for summoner info with the follow params:', summonerName, region);
+        var promise =  summonerInfoService.summoner(region, summonerName);
+        promise.then(function(data) {
+            summoner.summonerData = data;
+            console.log('In de controller', data);
+        }).catch(function(errorResponse) {
+            console.log('Error loading summoner info', errorResponse);
         });
+    }
 
-    }]
-)
-;
+    $scope.$watch('summoner.regionCodeInput', function (newRegion, oldRegion) {
+        console.log('Region changed from', oldRegion, 'to', newRegion);
+        stateService.setActiveRegion(newRegion);
+    });
+
+}
