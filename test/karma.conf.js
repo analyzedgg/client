62603ef06bf906249f0267d9f3866c638214ff6e
Martin Kanters <Martin.Kanters@infosupport.com>
2016-08-03 20:12:44 +0200

    Updated versions and getting highcharts from bower

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -6,15 +6,19 @@ module.exports = function (config) {
         frameworks: ['jasmine'],
 
         files: [
+            'src/lib/html5-boilerplate/js/vendor/modernizr-2.6.2.min.js',
             'src/lib/jquery/dist/jquery.js',
             'src/lib/angular/angular.js',
-            'src/lib/angular-ui-router/release/angular-ui-router.js',
             'src/lib/angular-resource/angular-resource.js',
             'src/lib/angular-mocks/angular-mocks.js',
+            'src/lib/angular-ui/build/angular-ui.js',
+            'src/lib/angular-bootstrap/ui-bootstrap.js',
             'src/lib/bootstrap/dist/js/bootstrap.js',
+            'src/lib/angular-ui-router/release/angular-ui-router.js',
+            'src/lib/highcharts/highstock.src.js',
             'src/lib/highcharts-ng/dist/highcharts-ng.js',
             'src/app/**/*.module.js',
-            'src/app/**/!(highcharts-theme).js',
+            'src/app/**/*.js',
 
             // Test files
             'test/unit/**/*.spec.js'
