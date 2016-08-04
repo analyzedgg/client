01a51be8eef7816c1c9f6b4b527976323377c196
Martin Kanters <mkanters93@gmail.com>
2016-08-04 19:31:41 +0200

    Fixed the matchHistoryService unit-test

--- test/unit/summoner/matchHistory.service.spec.js
+++ test/unit/summoner/matchHistory.service.spec.js
@@ -1,41 +1,55 @@
 describe('MatchHistoryService', function () {
-
     var matchHistoryService,
-        httpBackend;
+        httpBackend,
+        ENV,
+        testMatches = [{
+            "queueType": "RANKED_SOLO_5x5",
+            "matchDuration": 2375,
+            "stats": {
+                "minionKills": 4
+            }
+        }, {
+            "queueType": "RANKED_SOLO_5x5",
+            "matchDuration": 1251,
+            "stats": {
+                "minionKills": 3
+            }
+        }],
+        testFilteredMatches = [],
+        matchesFilterService = {
+            filter: jasmine.createSpy("matchesFilterServiceSpy").and.returnValue(testFilteredMatches)
+        };
+
+
     beforeEach(module('leagueApp.service'));
-    beforeEach(inject(function (_MatchHistoryService_) {
-        matchHistoryService = _MatchHistoryService_;
-        // httpBackend = $httpBackend;
-
-        console.log(matchHistoryService, httpBackend);
-
+    beforeEach(module(function ($provide) {
+        $provide.value('MatchesFilterService', matchesFilterService);
     }));
+    beforeEach(inject(function (_MatchHistoryService_, $httpBackend, _ENV_) {
+            matchHistoryService = _MatchHistoryService_;
+            httpBackend = $httpBackend;
+            ENV = _ENV_;
+        }
+    ));
 
     //Must
     describe('the handling of match history data', function () {
-        it('test', function() {
-            console.log('gwat');
+        it('should retrieve the right information given an used summoner id', function(done) {
+            httpBackend.whenGET(ENV.BASE_URL + '/api/euw/matchhistory/12345').respond(testMatches);
+
+            matchHistoryService.matchHistory('euw', '12345')
+                .then(function (data) {
+                    expect(matchesFilterService.filter).toHaveBeenCalledWith(data.raw);
+
+                    expect(data.raw.length).toBe(testMatches.length);
+                    expect(data.raw[0].queueType).toEqual(testMatches[0].queueType);
+                    expect(data.raw[1].stats.minionKills).toEqual(testMatches[1].stats.minionKills);
+                    expect(data.filtered).toEqual(testFilteredMatches)
+                }).catch(function(error) {
+                    done.fail(error);
+                }).finally(done);
+
+            httpBackend.flush();
         });
-        // it('should retrieve the right information given an used summoner id', function () {
-        //     var testMatches = [{
-        //         "queueType": "RANKED_SOLO_5x5",
-        //         "matchDuration": 2375,
-        //         "stats": {
-        //             "minionKills": 4
-        //         }
-        //     }, {
-        //         "queueType": "RANKED_SOLO_5x5",
-        //         "matchDuration": 1251,
-        //         "stats": {
-        //             "minionKills": 3
-        //         }
-        //     }];
-        //
-        //     httpBackend.whenGET('api/euw/matchhistory/12345').respond(testMatches);
-        //     matchHistoryService.matchHistory('euw', '12345').then(function (data) {
-        //         expect(data).toBe(testMatches);
-        //     });
-        //
-        // });
     });
 });
